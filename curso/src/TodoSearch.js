3ea21934d028f47118265ff3b593428c225a6e85
JohanaLl <johisllanos@gmail.com>
2023-05-07 17:54:39 -0500

    Eventos: onClick, onChange

--- curso/src/TodoSearch.js
+++ curso/src/TodoSearch.js
@@ -3,7 +3,16 @@ import './TodoSearch.css';
 
 function TodoSearch () {
     return (
-        <input placeholder="Cebolla" className="TodoSearch"/>
+        <input 
+            placeholder="Cebolla" 
+            className="TodoSearch"
+            onChange={(event) => {
+                console.log('Escribiste en el TodoSearch');
+                console.log(event);
+                console.log(event.target);
+                console.log(event.target.value);
+            }}
+        />
     );
 }
 
