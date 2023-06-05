f5b06b4a6c516ad63e7ebae751ab06dc1ae96b90
JohanaLl <johisllanos@gmail.com>
2023-06-05 06:13:48 -0500

    Efectos de React

--- curso/src/App/index.js
+++ curso/src/App/index.js
@@ -57,6 +57,30 @@ function App() {
     saveTodos(newTodos);
   }
 
+  console.log("Log 1");
+  /**Cuando la función useEffect no tiene más argumentos, lo que está dentro de la funcón
+   * se va a ejecutar siempre al final de cada render
+   */
+  // React.useEffect(() => {
+  //   console.log("Log 2");
+  // })
+
+  /**Cuando la función useEffect tiene como segundo argumento un array vacío la función solo se va a 
+   * ejecutar la primera vez del render, solo se ejecuta una sola vez
+ */
+  // React.useEffect(() => {
+  //   console.log("Log 2");
+  // }, [])
+
+  /**Cuando la función useEffect tiene como segundo argumento un array con información, quiere decir que se 
+   * ejecutará la primera vez del render y adicionalmente cada vez que se cambie el estado derivado que se 
+   * pasó como parámetro en el array en este caso completedTodos
+  */
+  React.useEffect(() => {
+    console.log("Log 2");
+  }, [completedTodos])
+  console.log("Log 3");
+
   return (
     <AppUI
       completedTodos={completedTodos}
