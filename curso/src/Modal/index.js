df712db818556569289aa73644c6c94341c93adc
JohanaLl <johisllanos@gmail.com>
2023-07-09 16:30:37 -0500

    estados para abrir y cerrar un modal

--- curso/src/Modal/index.js
+++ curso/src/Modal/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import './Modal.css'
 
 function Modal({ children }) {
     //Lo que se quiere teletransportar
   return ReactDOM.createPortal(
-    <div className="Modal">
+    <div className="ModalBackground">
       {children}
     </div>,
     // a donde se quiere teletransportar
